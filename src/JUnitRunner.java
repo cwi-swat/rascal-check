5f08fdf25714e34c25e7a2ad5c25ba03720a1616
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2013-01-07 22:19:49 +0100

    almost got eclipse running, except workspace is closed

--- src/JUnitRunner.java
+++ src/JUnitRunner.java
@@ -1,12 +1,26 @@
 import org.junit.runner.RunWith;
+import org.rascalmpl.eclipse.console.RascalScriptInterpreter;
+import org.rascalmpl.interpreter.Evaluator;
+import org.rascalmpl.test.infrastructure.IRascalJUnitTestSetup;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
-import org.rascalmpl.test.infrastructure.RascalJUnitTestRoot;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
-import org.rascalmpl.test.infrastructure.RascalJUnitTestScheme;
-
+import org.rascalmpl.uri.ClassResourceInputOutput;
+import org.rascalmpl.uri.URIResolverRegistry;
+import org.rascalmpl.uri.URIUtil;
 
 @RunWith(RascalJUnitTestRunner.class)
 @RascalJUnitTestPrefix("tests")
-@RascalJUnitTestScheme("rascal-tests")
-//@RascalJUnitTestRoot("src")
-public class JUnitRunner { }
+public class JUnitRunner implements IRascalJUnitTestSetup {
+  @Override
+  public void setup(Evaluator eval) {
+    URIResolverRegistry resolverRegistry = eval.getResolverRegistry();
+    resolverRegistry.registerInput(new ClassResourceInputOutput(resolverRegistry, "rascal-tests", getClass(), ""));
+    eval.addRascalSearchPath(URIUtil.rootScheme("rascal-tests"));
+    
+    ClassResourceInputOutput eclipseResolver = new ClassResourceInputOutput(resolverRegistry, "eclipse-std", RascalScriptInterpreter.class, "/org/rascalmpl/eclipse/library");
+    resolverRegistry.registerInput(eclipseResolver);
+    
+    eval.addRascalSearchPath(URIUtil.rootScheme(eclipseResolver.scheme()));
+    eval.addClassLoader(RascalScriptInterpreter.class.getClassLoader());
+  }
+}
