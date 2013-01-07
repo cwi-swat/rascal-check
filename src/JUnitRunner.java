39a3419f5bee636d7943541c34546a3b8986a8a5
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2013-01-07 22:03:22 +0100

    trying to get JUnit online

--- src/JUnitRunner.java
+++ src/JUnitRunner.java
@@ -1,8 +1,12 @@
 import org.junit.runner.RunWith;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestRoot;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
+import org.rascalmpl.test.infrastructure.RascalJUnitTestScheme;
 
 
 @RunWith(RascalJUnitTestRunner.class)
 @RascalJUnitTestPrefix("tests")
+@RascalJUnitTestScheme("rascal-tests")
+//@RascalJUnitTestRoot("src")
 public class JUnitRunner { }
