f3576331dc60f54e2c677c0c44affa69c6b3a0ff
Davy Landman <davy.landman@gmail.com>
2013-10-29 15:17:39 +0100

    Fixed compile error

--- src/JUnitRunner.java
+++ src/JUnitRunner.java
@@ -4,7 +4,7 @@ import org.rascalmpl.interpreter.Evaluator;
 import org.rascalmpl.test.infrastructure.IRascalJUnitTestSetup;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestPrefix;
 import org.rascalmpl.test.infrastructure.RascalJUnitTestRunner;
-import org.rascalmpl.uri.ClassResourceInputOutput;
+import org.rascalmpl.uri.ClassResourceInput;
 import org.rascalmpl.uri.URIResolverRegistry;
 import org.rascalmpl.uri.URIUtil;
 
@@ -14,10 +14,10 @@ public class JUnitRunner implements IRascalJUnitTestSetup {
   @Override
   public void setup(Evaluator eval) {
     URIResolverRegistry resolverRegistry = eval.getResolverRegistry();
-    resolverRegistry.registerInput(new ClassResourceInputOutput(resolverRegistry, "rascal-tests", getClass(), ""));
+    resolverRegistry.registerInput(new ClassResourceInput(resolverRegistry, "rascal-tests", getClass(), ""));
     eval.addRascalSearchPath(URIUtil.rootScheme("rascal-tests"));
     
-    ClassResourceInputOutput eclipseResolver = new ClassResourceInputOutput(resolverRegistry, "eclipse-std", RascalScriptInterpreter.class, "/org/rascalmpl/eclipse/library");
+    ClassResourceInput eclipseResolver = new ClassResourceInput(resolverRegistry, "eclipse-std", RascalScriptInterpreter.class, "/org/rascalmpl/eclipse/library");
     resolverRegistry.registerInput(eclipseResolver);
     
     eval.addRascalSearchPath(URIUtil.rootScheme(eclipseResolver.scheme()));
